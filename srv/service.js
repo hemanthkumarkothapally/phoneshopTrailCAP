60a3b160eaf9cccb69494ec73f7a5b8f6e5f1912
Hemanth Kumar Kothapally <hemanthkumarkothapally@gmail.com>
2025-02-20 07:27:52 +0000

    change

--- srv/service.js
+++ srv/service.js
@@ -1,66 +1,68 @@
 const { update } = require('@sap/cds');
 const cds = require('@sap/cds');
-module.exports = (src) =>{
-    src.on('msg', req=>`hello ${req.data.txt}`)
-    src.on('text', req=> `you have ${req.data.you} orders`)
+module.exports = (src) => {
+    src.on('msg', req => {
+        return "helo " + req.data.txt
+    })
+    src.on('text', req => `you have ${req.data.you} orders`)
 
-    const{  Product, Customer, Order } = cds.entities('sap.b1.phoneshop');
-
+    const { Product, Customer, Order } = cds.entities('sap.b1.phoneshop');
 
     // on is the event handler....
-    src.on('READ',"Product",async(req)=>{
-       console.log("This function is working");
-       const data = await SELECT.from(Product);
+    // it will directly push the data when product is called....
+    src.on('READ', "Product", async (req) => {
+        console.log("This function is working");
+        const data = await SELECT.from(Product);
 
-       const add =  {
-        "productID": 123,
-        "brand": "oppo",
-        "model": "y11",
-        "model_name":"oppo-y11",
-        "rom": 6,
-        "ram":128,
-        "price":12000.00
-      };
-       data.push(add);
-       return data;
+        const add = {
+            "productID": 123,
+            "brand": "oppo",
+            "model": "y11",
+            "model_name": "oppo-y11",
+            "rom": 6,
+            "ram": 128,
+            "price": 12000.00
+        };
+        data.push(add);
+        return data;
     });
 
-    
-    //adding data through test.http file For PRODUCTS
-    //event handlers
+
+    //adding data through test.http file ......
+    //post call event 
     src.on('CREATE', "Customer", async (req) => {
-    console.log("Creating a new product...");
+        console.log("Creating a new product...");
 
-    const { customerID,name,mobile_no} = req.data;
+        const { customerID, name, mobile_no } = req.data;
 
-    const newcustomer = {
-        customerID,
-        name,
-        mobile_no
-        
-    };
+        const newcustomer = {
+            customerID,
+            name,
+            mobile_no
 
-    // Insert the new product into the database
-    await INSERT.into(Customer).entries(newcustomer);
+        };
 
-    return newcustomer;
+        // Insert the new product into the database
+        await INSERT.into(Customer).entries(newcustomer);
+
+        return newcustomer;
     });
 
-    src.on('READ',"Order", async(req)=>{
-        try{
+    src.on('READ', "Order", async (req) => {
+        try {
             const wholedata = await SELECT.from(Order);
             return wholedata;
         } catch (error) {
-            return req.reject(500,'not getting data : ' + error.message);
+            return req.reject(500, 'not getting data : ' + error.message);
         }
     });
 
-    src.on('CREATE',"Order",async(req)=>{
-        const {orderID, orderDate, amount, customerID} = req.data;
-        if(!orderID||!orderDate||!amount||!customerID){
-            return req.reject(400,"all fields are mandatory");   
+    src.on('CREATE', "Order", async (req) => {
+        const { orderID, orderDate, amount, customerID } = req.data;
+        if (!orderID || !orderDate || !amount || !customerID) {
+            return req.reject(400, "all fields are mandatory");
         }
-        const neworder={
+        const neworder = {
             orderID,
             orderDate,
             amount,
@@ -70,55 +72,54 @@ module.exports = (src) =>{
             await INSERT.into(Order).entries(neworder);
             return neworder;
         } catch (error) {
-            return req.reject(500,'not inserting : '+ error.message);
+            return req.reject(500, 'not inserting : ' + error.message);
         }
-        
+
     });
-    src.on('UPDATE',"Order",async(req)=>{
-        const {orderID, orderDate, amount,customerID} = req.data;
-        if(!orderID){
-            return req.reject(400,"orderID not given");
+    src.on('UPDATE', "Order", async (req) => {
+        const orderID = req.data.orderID;
+        if (!orderID) {
+            return req.reject(400, "orderID not given");
         }
-        const updatedorder ={orderID, orderDate, amount, customerID};
+        const updatedorder = req.data;
         try {
-            await UPDATE(Order).set(updatedorder).where({orderID});
+            await UPDATE(Order).set(updatedorder).where({ orderID });
             return updatedorder;
-            
+
         } catch (error) {
-            return req.reject(500,'unable to update: '+ error.message);
+            return req.reject(500, 'unable to update: ' + error.message);
         }
     });
 
-    src.on('DELETE',"Order", async(req)=>{
-        const {orderID} = req.data;
-        if(!orderID){
-            return req.reject(400,'Order ID not entered');
+    src.on('DELETE', "Order", async (req) => {
+        const { orderID } = req.data;
+        if (!orderID) {
+            return req.reject(400, 'Order ID not entered');
         }
         try {
-            const deletedorder= await DELETE.from(Order).where({orderID});
-            if(deletedorder===0){
-                return req.reject(404,'Order not found');
+            const deletedorder = await DELETE.from(Order).where({ orderID });
+            if (deletedorder === 0) {
+                return req.reject(404, 'Order not found');
             }
-            return `U`;
         } catch (error) {
-            return req.reject(500,'Not Able to Delete: ' + error.message);
+            return req.reject(500, 'Not able to Delete: ' + error.message);
         }
     });
 
-    src.on('CREATE',"Product", async(req)=>{
-        const {productID,brand,model,rom,ram,price} = req.data;
+    src.on('CREATE', "Product", async (req) => {
+        const { productID, brand, model, rom, ram, price } = req.data;
         console.log("product is posted");
-        if(!productID || !brand || !model || !rom || !ram || !price){
+        if (!productID || !brand || !model || !rom || !ram || !price) {
             return req.reject(400, 'All Fields Are Mandatory');
         }
-        const newproduct={
-            productID,brand,model,rom,ram,price
+        const newproduct = {
+            productID, brand, model, rom, ram, price
         }
         try {
             await INSERT.into(Product).entries(newproduct);
             return newproduct;
         } catch (error) {
-            return req.reject(500,'Unable To Push the data'+ error.message);
+            return req.reject(500, 'Unable To Push the data' + error.message);
         }
     });
 
